0384c7210190594066fcb69d798088791e9f9019
wisnubjoey <wisnuhutama@outlook.com>
2024-11-14 09:45:00 +0800

    Update repository with latest changes

--- ap.js
+++ ap.js
@@ -132,3 +132,16 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+let menu = document.querySelector('#menu-icon');
+let nav = document.querySelector('.nav');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    nav.classList.remove('open');
+};
